08028689536480a6620545089aca217715522c30
Salman Aziz <salmanazizshuvo@gmail.com>
2024-12-04 10:50:08 +0600

    refactor: update color code of homeScreenHeadingColor

--- lib/styles/color/colors.dart
+++ lib/styles/color/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const white = Colors.white;
   static const classroomColor = Color(0xFFF5F5F5);
-  static const homeScreenHeadingColor = Color(0xFFF5F5F5);
+  static const homeScreenHeadingColor = Color(0xFF1F1F39);
   static const classroomSecondaryColor = Color(0xFF858597);
   static const btnSecondaryColor = Color(0xFF5177FF);
   static const classroomPrimaryColor = Color(0xFF383839);
